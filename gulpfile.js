34ce32bc0e11c84d6e7457d99a098f4d2c4d7301
Johnsen <296240671@qq.com>
2017-09-25 17:02:47 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,38 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const browserSync = require('browser-sync').create();
+const nodemon = require('gulp-nodemon');
 
-gulp.task('babel', () => {
-  return gulp.src('public/javascripts/typed.js')
-    .pipe(babel({
-      presets: ['env']
-    }))
-    .pipe(gulp.dest('public/javascripts/es5'))
+// gulp.task('babel', () => {
+//   return gulp.src('public/javascripts/*.js')
+//     .pipe(babel({
+//       presets: ['env']
+//     }))
+//     .pipe(gulp.dest('public/javascripts/es5'))
+// });
+
+gulp.task('nodemon', () => {
+  return nodemon({
+    script: 'bin/www',
+    env: { 'NODE_ENV': 'development' }
+  })
+});
+
+gulp.task('dev', ['nodemon'], () => {
+  browserSync.init({
+    proxy: 'http://localhost:3000',
+    browser: 'chrome',
+    port: 8080,
+    open: false
+    
+  });
+  gulp.watch(['./views/*', './public/*'], ['file-watch'])
+    // .on('change', browserSync.reload());
 })
 
-gulp.task('default', ['babel'])
+gulp.task('file-watch', (done) => {
+  browserSync.reload();
+  done();
+})
+
+gulp.task('default', ['dev'])
